65a52284d34560ca6d235248c7f21a8372000338
athrey-navda <athu.navada@gmail.com>
2024-09-10 23:25:00 -0400

    update: api url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://api-my-blog.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
